9709a404b24882b07f74b8fb215d69a1fb184b50
maoqin <ucabm57@ucl.ac.uk>
2024-12-18 15:23:18 +0000

    增加日志

--- pipeline_script.py
+++ pipeline_script.py
@@ -3,33 +3,29 @@ from subprocess import Popen, PIPE
 import glob
 import os
 import multiprocessing
+import logging
 
-"""
-usage: python pipeline_script.py [INPUT DIR] [OUTPUT DIR]
-approx 5seconds per analysis
-"""
+# Set up logging
+logging.basicConfig(filename='pipeline.log', level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 
 def run_parser(input_file, output_dir):
-    """
-    Run the results_parser.py over the hhr file to produce the output summary
-    """
     search_file = input_file+"_search.tsv"
-    print(search_file, output_dir)
+    logging.info(f'Running parser on {search_file} with output directory {output_dir}')
     cmd = ['python', './results_parser.py', output_dir, search_file]
-    print(f'STEP 2: RUNNING PARSER: {" ".join(cmd)}')
-    p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
+    logging.info(f'STEP 2: RUNNING PARSER: {" ".join(cmd)}')
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate()
-    print(out.decode("utf-8"))
+    logging.info(out.decode("utf-8"))
+    if err:
+        logging.error(err.decode("utf-8"))
 
 def run_merizo_search(input_file, id):
-    """
-    Runs the merizo domain predictor to produce domains
-    """
     cmd = ['python3',
            '/home/almalinux/merizo_search/merizo_search/merizo.py',
            'easy-search',
            input_file,
-           '/home/almalinux/merizo_search/examples/database/cath-4.3-foldclassdb',
+           '/home/almalinux/data/cath',
            id,
            'tmp',
            '--iterate',
@@ -39,15 +35,15 @@ def run_merizo_search(input_file, id):
            '--threads',
            '1'
            ]
-    print(f'STEP 1: RUNNING MERIZO: {" ".join(cmd)}')
-    p = Popen(cmd, stdin=PIPE,stdout=PIPE, stderr=PIPE)
+    logging.info(f'STEP 1: RUNNING MERIZO: {" ".join(cmd)}')
+    p = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
     out, err = p.communicate()
-    
+    logging.info(out.decode("utf-8"))
+    if err:
+        logging.error(err.decode("utf-8"))
+
 def read_dir(input_dir):
-    """
-    Function reads a fasta formatted file of protein sequences
-    """
-    print("Getting file list")
+    logging.info(f'Reading directory {input_dir}')
     file_ids = list(glob.glob(input_dir+"*.pdb"))
     analysis_files = []
     for file in file_ids:
@@ -56,14 +52,10 @@ def read_dir(input_dir):
     return(analysis_files)
 
 def pipeline(filepath, id, outpath):
-    # STEP 1
     run_merizo_search(filepath, id)
-    # STEP 2
     run_parser(id, outpath)
 
 if __name__ == "__main__":
     pdbfiles = read_dir(sys.argv[1])
     p = multiprocessing.Pool(1)
     p.starmap(pipeline, pdbfiles[:10])
-
-        
